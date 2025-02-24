05d23baa88322b45f937bf4654b42f1cf7b23b36
Vivek Mengu <vivek.mengu@Viveks-MacBook-Pro.local>
2025-02-24 20:32:19 +0530

    fixed ReactDOM in all instances

--- packages/react-select/src/internal/NodeResolver.js
+++ packages/react-select/src/internal/NodeResolver.js
@@ -12,8 +12,8 @@ export default class NodeResolver extends Component<Props> {
   componentDidMount() {
 
     ReactDOM.findDOMNode
-      ? this.props.innerRef(reactDom.findDOMNode(this))
-      : this.props.innerRef(reactDom.__DOM_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE.findDOMNode(this));
+      ? this.props.innerRef(ReactDOM.findDOMNode(this))
+      : this.props.innerRef(ReactDOM.__DOM_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE.findDOMNode(this));
   }
   componentWillUnmount() {
     this.props.innerRef(null);
